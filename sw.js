51a71aa39797d6cf6b03078a1caf28d7cb2678c3
caristim <carlosar@adinet.com.uy>
2026-05-17 15:42:25 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,4 @@
+// sw.js - Service Worker para OneSignal
+// OneSignal maneja las notificaciones push desde el servidor.
+// Este archivo es requerido por OneSignal para funcionar correctamente.
+importScripts('https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js');
