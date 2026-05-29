06e6fceb419db389545670fd3d733acf1259c8c6
caristim <carlosar@adinet.com.uy>
2026-05-28 21:38:58 -0300

    Implement service worker install and activate events
    Added event listeners for install and activate phases.

--- sw.js
+++ sw.js
@@ -1,4 +1,9 @@
-// sw.js - Service Worker para OneSignal
-// OneSignal maneja las notificaciones push desde el servidor.
-// Este archivo es requerido por OneSignal para funcionar correctamente.
 importScripts('https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js');
+
+self.addEventListener('install', event => {
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(self.clients.claim());
+});
